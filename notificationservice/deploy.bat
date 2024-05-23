a4427afb530a3ace7ce743736093cc68120c2bd9
shantanuprx <shantanu.kumar@perennialsys.com>
2024-05-23 08:19:33 +0530

    Transferring properties to centralized config server. Adding deployment related files.

---
+++ notificationservice/deploy.bat
@@ -0,0 +1,1 @@
+mvn clean install -Dmaven.test.skip=true & docker build . -t gryffindor937/notificationservicedeployment:1.0 & docker push gryffindor937/notificationservicedeployment:1.0 & kubectl delete deployment notificationservicedeployment  & kubectl apply -f deployment.yml
